08e51a96ccf954f64c546f3e6ed81a961866f900
RahulBoggaram <boggaram.rahul2000@gmail.com>
2020-11-22 21:07:32 +0530

    flutter intro

---
+++ android/app/src/main/kotlin/com/flutter_app3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter_app3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: 
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
